22513e9466bc8665ccfde0d901c80ad1f6114cd7
Damion Morgan <only4school5680@gmail.com>
2024-03-25 16:58:34 -0400

    basic home page layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatChipsModule,MatTabsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
