1e8f49b1882662c6ebf1cf12e2b163204de850d3
Damion Morgan <only4school5680@gmail.com>
2024-03-25 15:49:53 -0400

    got rid of the contact page since it is now in the footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
 import { CartComponent } from './cart/cart.component';
 import { SettingsComponent } from './settings/settings.component';
 export const routes: Routes = [
@@ -8,11 +7,6 @@ export const routes: Routes = [
         path:'',
         component:HomeComponent,
         title:"Home Page"
-    },
-    {
-        path:'contact',
-        component:ContactComponent,
-        title:'Contact Page'
     },
     {
         path:'cart',
