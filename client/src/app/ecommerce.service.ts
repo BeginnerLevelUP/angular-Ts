4b2029b62751346b04b342c80bc258b3126f4427
Damion Morgan <only4school5680@gmail.com>
2024-04-09 20:36:54 -0400

    creating a ecommerce serivce now time to create the routes

--- client/src/app/ecommerce.service.ts
+++ client/src/app/ecommerce.service.ts
@@ -1,13 +1,34 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject } from '@angular/core';
-import { Router } from '@angular/router';
-import { jwtDecode } from 'jwt-decode';
-import { BehaviorSubject, Observable, tap } from 'rxjs';
-import { User } from './user';
+import { Injectable, signal } from '@angular/core';
+import { Product } from './product';
+import { Cart } from './cart';
 @Injectable({
   providedIn: 'root'
 })
 export class EcommerceService {
+  private url = 'http://localhost:5200';
+  products$ = signal<Product[]>([]);
+  product$ = signal<Product>({} as Product);
+  constructor(private httpClient: HttpClient) { }
 
-  constructor() { }
+
+  refreshProducts(){
+    this.httpClient.get<Product[]>(`${this.url}/api/products`)
+      .subscribe(products => {
+        this.products$.set(products);
+      });
+  }
+  
+  getProducts(){
+    this.refreshProducts();
+    return this.products$();
+  }
+
+  getProduct(id: string){
+    this.httpClient.get<Product>(`${this.url}/api/products/${id}`).subscribe(product => {
+    this.product$.set(product);
+    return this.product$();
+    });
+  }
+
 }
